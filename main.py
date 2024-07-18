9709ccc1cd05a9e6318490ac0c4a860069b38d4b
oboard <oboard@outlook.com>
2024-07-18 21:57:27 +0800

    fix: port

--- main.py
+++ main.py
@@ -313,4 +313,4 @@ def delete(id):
 
 if __name__ == '__main__':
     # app.run(host='0.0.0.0', port=5000, debug=True)
-    app.run(host='0.0.0.0', port=5001)
+    app.run(host='0.0.0.0', port=5000)
